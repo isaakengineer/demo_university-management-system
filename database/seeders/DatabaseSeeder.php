ef6e74d277606a5f149a305fb1338ef6379c1280
Isaak Engineer <i-engineer@tutamail.com>
2025-07-25 19:48:16 +0330

    Migrations und Seeder Integriert und Funktioniert

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,6 +6,7 @@ use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
+use UniversityManagementSystem\ApprovalManagementSystem\Database\Seeders\DatabaseSeeder as ApprovalSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -21,6 +22,10 @@ class DatabaseSeeder extends Seeder
             CourseSeeder::class,
             SemesterEnrollmentSeeder::class,
             CourseEnrollmentSeeder::class,
+        ]);
+
+        $this->call([
+            ApprovalSeeder::class
         ]);
     }
 }
